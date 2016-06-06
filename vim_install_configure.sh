93d6c1c3c4c11fa5586fbc403b390972f5a039a5
glamas <glamas@163.com>
2016-06-06 23:09:41 +0800

    添加vim编译参数文件

---
+++ vim_install_configure.sh
@@ -0,0 +1,19 @@
+./configure --prefix=/usr/local \
+--with-features=huge \
+--enable-luainterp=dynamic \
+--enable-perlinterp=dynamic \
+--enable-pythoninterp=dynamic \
+--enable-python3interp=dynamic \
+--enable-tclinterp=dynamic \
+--enable-rubyinterp=dynamic \
+--enable-cscope \
+--enable-multibyte \
+--enable-xim \
+--enable-fontset \
+--with-x 
+
+
+#--enable-gui=auto 
+#--enable-gnome-check 
+
+#--enable-gui=no
