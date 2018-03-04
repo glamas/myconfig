aeebf4a9f8aa1e029c3f28515f0edb74acf18ceb
glamas <glamas@163.com>
2018-03-04 17:09:13 +0800

    update

--- vim_install_configure.sh
+++ vim_install_configure.sh
@@ -17,3 +17,25 @@
 #--enable-gnome-check 
 
 #--enable-gui=no
+
+https://gist.github.com/odiumediae/3b22d09b62e9acb7788baf6fdbb77cf8
+./configure \
+--enable-multibyte \
+--enable-perlinterp=dynamic \
+--enable-rubyinterp=dynamic \
+--with-ruby-command=/usr/local/bin/ruby \
+--enable-pythoninterp=dynamic \
+--with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+--enable-python3interp \
+--with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+--enable-luainterp \
+--with-luajit \
+--enable-cscope \
+--enable-gui=auto \
+--with-features=huge \
+--with-x \
+--enable-fontset \
+--enable-largefile \
+--disable-netbeans \
+--with-compiledby="yourname" \
+--enable-fail-if-missing
