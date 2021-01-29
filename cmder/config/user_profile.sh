a0c866a22a047553949a9acb33e0f13b71d875d8
glamas <lzgug2@outlook.com>
2021-01-29 18:42:32 +0800

    Update user_profile.sh

--- cmder/config/user_profile.sh
+++ cmder/config/user_profile.sh
@@ -10,3 +10,16 @@ HOME="${CMDER_ROOT}/home"
 HISTFILE="${HOME}/.bash_history"
 #PATH=${PATH}:${CMDER_ROOT}/bin/python37
 . $HOME/.bashrc
+
+export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node
+# add $CMDER_ROOT/bin to path
+for file in `ls ${CMDER_ROOT}/bin`;
+do
+	if [ -d "${CMDER_ROOT}/bin/$file" ]; then
+		PATH=${CMDER_ROOT}/bin/$file:$PATH
+		if [ -d "${CMDER_ROOT}/bin/$file/bin" ]; then
+			PATH=${CMDER_ROOT}/bin/$file/bin:$PATH
+		fi
+	fi
+done
+export PATH=${CMDER_ROOT}/bin:$PATH
