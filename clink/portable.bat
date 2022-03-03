f4aca712a6df685a9fc5291a4d293227d626cee6
glamas <lzgug2@outlook.com>
2022-03-03 10:43:37 +0800

    Update portable.bat

--- clink/portable.bat
+++ clink/portable.bat
@@ -1,30 +1,4 @@
 @echo OFF
-setlocal enableextensions enabledelayedexpansion
-
-:: path
-SET portable_path=%~dp0clink;%~dp0config
-CALL :cascade_add_path %~dp0bin
-::----------------------------------------
-:: custom path
-REM CALL :cascade_add_path D:\Portable\ConEmu\bin
-CALL :cascade_add_path D:\Portable\msys64\clang64
-CALL :cascade_add_path D:\Portable\msys64\usr
-CALL :cascade_add_path D:\Portable\Git\usr\
-::----------------------------------------
-
-SET PATH=%portable_path%;%PATH%
-SET CLINK_PATH=%CLINK_PATH%;%~dp0config\clink-completions;%~dp0config\clink-flex-prompt
-
-:: home
-REM SET USERPROFILE=%~dp0home
-SET HOME=%~dp0home
-SET LANG=zh_CN.UTF-8
-
-::alias
-@doskey cd=cdh $*
-@doskey ls=ls --color $*
-@doskey ll=ls --color -al $*
-
 
 :: loader
 if /i "%processor_architecture%"=="x86" (
@@ -37,15 +11,3 @@ if /i "%processor_architecture%"=="x86" (
     )
 )
 cmd.exe /s /k ""%loader%" inject --profile "%~dp0\config" %1 %2 %3 %4 %5 %6 %7 %8 %9"
-goto :eof
-
-
-:cascade_add_path
-SET portable_path=!portable_path!;"%~1"
-FOR /d %%A IN (%~1\*) DO (
-	SET portable_path=!portable_path!;"%%A"
-	IF EXIST %%A\bin (
-		SET portable_path=!portable_path!;"%%A\bin"
-	)
-)
-EXIT /b 0
